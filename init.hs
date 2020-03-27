08984fba4a7434263383903694adb702f8b247a3
Takashi Saito <takashi11171117@gmail.com>
2020-03-28 02:49:49 +0900

    add recursion

--- init.hs
+++ init.hs
@@ -1,1 +1,27 @@
 doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = (if x > 100 then x else x * 2)
+
+-- パターンマッチ
+-- 再帰
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replica' :: Int -> a -> [a]
+replica' n x
+    | n <= 0 = []
+    | otherwise = x : replica' (n - 1) x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = [a | a <- xs, a <= x]
+        larger = [a | a <- xs, a > x]
+    in quicksort smallerOrEqual ++ [x] ++ quicksort larger
